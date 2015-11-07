95d50b63cdc7fd0015fca7a6b21ae8b90d0af638
Jane Gerashchenko <jane.gerashchenko@gmail.com>
2015-11-06 17:15:04 -0700

    project architecture change and fix for chrome driver config

---
+++ src/main/java/testconfigs/AtToDoMVCWithClearedDataAfterEachTest.java
@@ -0,0 +1,24 @@
+package testconfigs;
+
+import core.BrowserConfigAndReportedScreenshotsPerTest;
+import org.junit.After;
+import org.junit.Before;
+
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static com.codeborne.selenide.Selenide.open;
+import static pages.ToDoMVCPage.openToDoMVCPage;
+
+
+public class AtToDoMVCWithClearedDataAfterEachTest extends BrowserConfigAndReportedScreenshotsPerTest {
+
+    @Before
+    public void loadToDoMVCPage() {
+        openToDoMVCPage();
+    }
+
+    @After
+    public void clearData() {
+        executeJavaScript("localStorage.clear()");
+        open("http://todomvc.com");
+    }
+}
